53cc4ed0e81fb82e25b6ca89c3320e5e37b246bc
Justin Pinero <justinpinero@gmail.com>
2017-06-07 19:13:13 -0400

    All but Extra

--- src/selector.js
+++ src/selector.js
@@ -1,17 +1,50 @@
 var traverseDomAndCollectElements = function(matchFunc, startEl) {
   var resultSet = [];
+  console.log("This is the Arg: ", arguments[0]);
+  if(typeof arguments[0] === 'string'){
+    var selector = arguments[0].slice(arguments[0].indexOf(" ")+1);
+    //var selectorTypeMatch = selectorTypeMatcher(selector);
+    var newMatchFunc = matchFunctionMaker(selector);
+    var newStartEl = arguments[0].slice(0,arguments[0].indexOf(" "));
+    console.log("here");
+    traverseDomAndCollectElements(newMatchFunc, newStartEl);
+  }
+
+
 
   if (typeof startEl === "undefined") {
     startEl = document.body;
   }
-
+  traverse(matchFunc, startEl, resultSet);
   // traverse the DOM tree and collect matching elements in resultSet
   // use matchFunc to identify matching elements
-
-
-
   return resultSet;
 };
+
+function traverse(matchFunc, startEl, arr){
+  if(matchFunc(startEl)){
+    arr.push(startEl);
+  }
+  var children = startEl.children;
+  if(children){
+    for(var i=0;i<children.length; i++){
+      traverse(matchFunc, children[i], arr);
+    }
+  }
+}
+// function breadthFirst(matchFunc, startEl){
+//   var queue = [];
+//   queue.push(startEl);
+//   while(queue.length>0){
+//     if(queue[0].children){
+//       for(var i = 0; i<queue[0].children.length; i++){
+//         queue.push(queue[0].children[i]);
+//       }
+//
+//     }
+//   }
+// }
+
 
 
 // detect and return the type of selector
@@ -55,7 +88,6 @@ var matchFunctionMaker = function(selector) {
 
   } else if (selectorType === "tag.class") {
     matchFunction = function(element) {
-      console.log(element.tagName.toLowerCase(), selector.toLowerCase());
       let tagBool = element.tagName && (element.tagName.toLowerCase() ===
       selector.slice(0,selector.indexOf(".")).toLowerCase());
       let classBool = false;
@@ -83,4 +115,3 @@ var $ = function(selector) {
   elements = traverseDomAndCollectElements(selectorMatchFunc);
   return elements;
 };
-
